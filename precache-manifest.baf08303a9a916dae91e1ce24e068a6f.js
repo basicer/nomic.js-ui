97f2c85599eeeaf861afe7a03a237aa3d08d92a4
basicer <basicer@basicer.com>
2020-04-08 05:05:33 +0000

    Deploying to gh-pages from master @ 8a1f5e953c413fca2bee5593ff322b25d28c60e4 🚀

---
+++ precache-manifest.baf08303a9a916dae91e1ce24e068a6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5118e95dc8dbdc978dcfc476b89b15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73220219854d20afcb74",
+    "url": "/static/css/main.73c9cecb.chunk.css"
+  },
+  {
+    "revision": "496d2b196d9235cc3a12",
+    "url": "/static/js/2.b42b1d9f.chunk.js"
+  },
+  {
+    "revision": "73220219854d20afcb74",
+    "url": "/static/js/main.ca465af9.chunk.js"
+  },
+  {
+    "revision": "bc7615e31f6ee92c18d9",
+    "url": "/static/js/runtime-main.b33eb1b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
