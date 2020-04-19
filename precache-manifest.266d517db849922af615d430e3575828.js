f6c48c1c8688e9d11b7583d937e0e783df01096e
basicer <basicer@basicer.com>
2020-04-19 08:01:04 +0000

    Deploying to gh-pages from master @ 18beafe4b354e8ff22a2532492f5fc63e30c28a2 🚀

---
+++ precache-manifest.266d517db849922af615d430e3575828.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2c5b7dc70f56a0767b82630df3c77f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "325c3f89d4c2a4b2382b",
+    "url": "/static/css/main.73c9cecb.chunk.css"
+  },
+  {
+    "revision": "89d6dfecbc73a6b0e8ea",
+    "url": "/static/js/2.f70346ac.chunk.js"
+  },
+  {
+    "revision": "c84f4739af41c89acf61280cf3e5ab2b",
+    "url": "/static/js/2.f70346ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa1a3d798d1092f0505",
+    "url": "/static/js/3.373bc81c.chunk.js"
+  },
+  {
+    "revision": "05e0f0fee31aab7bb6bb",
+    "url": "/static/js/4.c54b3918.chunk.js"
+  },
+  {
+    "revision": "2053f1f84068a8a5f8f6",
+    "url": "/static/js/5.ad01016f.chunk.js"
+  },
+  {
+    "revision": "0b7039834a4486b89933",
+    "url": "/static/js/6.c61241b5.chunk.js"
+  },
+  {
+    "revision": "325c3f89d4c2a4b2382b",
+    "url": "/static/js/main.a2e27a79.chunk.js"
+  },
+  {
+    "revision": "09edcce44307331925bd",
+    "url": "/static/js/runtime-main.1714a20f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e67269ab418a5378c97583eeb8e61c9.js"
+  "/precache-manifest.266d517db849922af615d430e3575828.js"
 );
 
 self.addEventListener('message', (event) => {
