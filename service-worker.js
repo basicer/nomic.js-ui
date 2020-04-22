d6bb9e914e9bf3b48660b498b61c60883730015d
basicer <basicer@basicer.com>
2020-04-22 08:29:31 +0000

    Deploying to gh-pages from master @ ce751f352b10d43bb11baee3999cc7ff983d0b4d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b041130a0e89e1c82a96004aa47ef5fa.js"
+  "/precache-manifest.afbce4f394aa3d000082e1e6840cba41.js"
 );
 
 self.addEventListener('message', (event) => {
